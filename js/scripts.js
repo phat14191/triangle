477e782adbd4a7b190aeaac62ff47e387ebee14c
Jun Fritz and Phat Nguyen <phat@>
2017-04-27 16:45:46 -0700

    add final equations.

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,29 @@ $(document).ready(function() {
     var side2 = $("input#side2").val();
     var side3 = $("input#side3").val();
 
-    if (
-      (side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)
-    ){
-      alert("not a triangle");
-    }
-    else if ((side1  === side2) && (side2 === side3)) {
+
+      if ((side1  === side2) && (side2 === side3)) {
     	// equilateral;
       alert("equilateral!");
-    }
-    else if (
-    ((side1  === side2) && !(side2 === side3)) ||
-    ((side2  === side3) && !(side3 === side1)) ||
-    ((side1  === side3) && !(side3 === side2))
-    ) {
-    	// isosceles;
-      alert("isosceles!");
-    }
-    else if (!(side1  === side2) && !(side2 === side3)) {
-    	// scalene;
-      alert("scalene!");
-    }
+      }
 
+      else if (
+      ((side1  === side2) && (side2 !== side3)) ||
+      ((side2  === side3) && (side3 !== side1)) ||
+      ((side1  === side3) && (side3 !== side2))
+      ) {
+        alert("isosceles!");
+      }
+
+      else if (side1  !== side2 || side2 !== side3 || side1 !== side3) {
+      	// scalene;
+        alert("scalene!");
+      }
+      else if (
+        side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2
+      ){
+        alert("not a triangle");
+      }
 
     event.preventDefault();
   });
