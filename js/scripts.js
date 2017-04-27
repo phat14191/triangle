fdb732f6c7ea9d3420b59f2fcf8c2f95c0bf93b5
Jun Fritz and Phat Nguyen <phat@>
2017-04-27 16:15:56 -0700

    add conditional statements and attach alert for each type of triangle.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $("form.triangle").submit(function(event) {
+    var side1 = $("input#side1").val();
+    var side2 = $("input#side2").val();
+    var side3 = $("input#side3").val();
+
+    if (
+      (side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)
+    ){
+      alert("not a triangle");
+    }
+    else if ((side1  === side2) && (side2 === side3)) {
+    	// equilateral;
+      alert("equilateral!");
+    }
+    else if (
+    ((side1  === side2) && !(side2 === side3)) ||
+    ((side2  === side3) && !(side3 === side1)) ||
+    ((side1  === side3) && !(side3 === side2))
+    ) {
+    	// isosceles;
+      alert("isosceles!");
+    }
+    else if (!(side1  === side2) && !(side2 === side3)) {
+    	// scalene;
+      alert("scalene!");
+    }
+
+
+    event.preventDefault();
+  });
+});
